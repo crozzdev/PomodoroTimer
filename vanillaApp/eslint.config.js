2dde3050e844734086a78b7fe64c819be736f4aa
CrozzDev <crozzdev95@outlook.com>
2024-09-05 21:09:46 -0500

    finished configuering linter and formatter (prettier

--- vanillaApp/eslint.config.js
+++ vanillaApp/eslint.config.js
@@ -1,7 +1,21 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
-    { languageOptions: { globals: globals.browser } },
+    {
+        languageOptions: {
+            globals: globals.browser,
+            ecmaVersion: 2021, // Supports modern ECMAScript features
+            sourceType: 'module' // Supports the use of `import` and `export`
+        },
+        plugins: {
+            prettier: prettierPlugin
+        },
+        rules: {
+            'no-console': 'warn', // Warns on `console.log`
+            'prettier/prettier': 'error' // Reports Prettier formatting issues as errors
+        }
+    },
     pluginJs.configs.recommended
 ];
